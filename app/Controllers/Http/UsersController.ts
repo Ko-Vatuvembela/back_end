6376404966a9af3d2efe092ab6f0152fa6ca878b
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-11 02:33:14 -0300

    chore: update semicolon configuration

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,9 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class UsersController {
   public createUser = async (ctx: HttpContextContract) => {
-    const { response } = ctx
+    const { response } = ctx;
 
-    response.ok({ user: 'created' })
-  }
+    response.ok({ user: 'created' });
+  };
 }

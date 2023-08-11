30aa77796e903c3c2f7c4b61c6dd4974336ddc0a
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-11 20:19:12 -0300

    feat: Add the User migration

--- database/migrations/1691726924746_users.ts
+++ database/migrations/1691726924746_users.ts
@@ -1,15 +1,20 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
 
 export default class extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = 'users';
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.string('username').primary(), table.string('')
-    })
+      table.string('username', 21).primary(),
+        table.string('nome', 45),
+        table.string('sobrenome', 45),
+        table.string('email', 45),
+        table.string('foto', 255),
+        table.string('password', 255);
+    });
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }

ab996a43e6d6ffb1c8d28709c5fe65c295f0a214
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-12 13:58:41 -0300

    feat #4: Add the language model

---
+++ app/Models/Lingua.ts
@@ -0,0 +1,10 @@
+import { DateTime } from 'luxon';
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
+
+export default class Lingua extends BaseModel {
+  @column({ isPrimary: true })
+  public idLingua: string;
+
+  @column()
+  public lingua: string;
+}

d97ca14c72aebc15914ffc4a665da438036e3b9e
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-12 14:11:41 -0300

    update #4: Add some  missing column

--- database/migrations/1691796013993_postagems.ts
+++ database/migrations/1691796013993_postagems.ts
@@ -7,12 +7,18 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.string('id_postagem', 64).primary(),
         table.string('utilizador_fk', 64),
-        table.text('conteudo'),
+        table.string('lingua_fk', 64);
+      table.text('conteudo'),
         table
           .foreign('utilizador_fk')
           .references('utilizadores.username')
           .onDelete('CASCADE')
           .onUpdate('CASCADE');
+      table
+        .foreign('lingua_fk')
+        .references('linguas.id_lingua')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
     });
   }
 

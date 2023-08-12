ca4b9282f7107fd850b6ae87124799b0b6ea9f56
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-12 14:12:25 -0300

    feat #4: Add the post model and update the language model

---
+++ app/Models/Postagem.ts
@@ -0,0 +1,23 @@
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm';
+import Utilizador from './Utilizador';
+import Lingua from './Lingua';
+
+export default class Postagem extends BaseModel {
+  @column({ isPrimary: true })
+  public idPostagem: string;
+
+  @column()
+  public conteudo: string;
+
+  @column()
+  public utilizadorFK: string;
+
+  @column()
+  public linguaFK: string;
+
+  @belongsTo(() => Utilizador, { foreignKey: 'utilizadorFK' })
+  public username: BelongsTo<typeof Utilizador>;
+
+  @belongsTo(() => Lingua, { foreignKey: 'linguaFK' })
+  public idLingua: BelongsTo<typeof Lingua>;
+}

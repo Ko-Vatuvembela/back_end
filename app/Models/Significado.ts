2f2fd1711fef7c83c59203d5116ded64c1436a9e
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-12 15:20:35 -0300

    feat #4: Add the meaning  model

---
+++ app/Models/Significado.ts
@@ -0,0 +1,34 @@
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm';
+import Lingua from './Lingua';
+import Utilizador from './Utilizador';
+import ClasseGramatical from './ClasseGramatical';
+import Palavra from './Palavra';
+
+export default class Significado extends BaseModel {
+  @column({ isPrimary: true })
+  public idSignificado: string;
+
+  @column()
+  public significado: string;
+
+  @column()
+  public exemplo: string;
+
+  @column()
+  public classeGramaticalFK: string;
+
+  @column()
+  public palavraFK: string;
+
+  @column()
+  public utilizadorFK: string;
+
+  @belongsTo(() => Utilizador, { foreignKey: 'utilizadorFK' })
+  public username: BelongsTo<typeof Utilizador>;
+
+  @belongsTo(() => ClasseGramatical, { foreignKey: 'classeGramaticalFK' })
+  public idClasseGramatical: BelongsTo<typeof ClasseGramatical>;
+
+  @belongsTo(() => Palavra, { foreignKey: 'palavraFK' })
+  public idPalavra: BelongsTo<typeof Palavra>;
+}

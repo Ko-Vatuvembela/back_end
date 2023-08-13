94a0eeccb27d0d4e0d7ac7509878ff9e611e8b38
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-13 01:09:34 -0300

    feat #4: Add the BaseValidator and Uservalidator

---
+++ app/Validators/BaseValidator.ts
@@ -0,0 +1,15 @@
+import { validator } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+export default class BaseValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public messages = {
+    minLength: 'O campo {{field}} deve conter no mínimo {{options.minLength}} caracteres',
+    maxLength: 'O campo {{field}} deve conter no máximo {{options.minLength}} caracteres',
+    required: 'O campo {{field}} é obrigatório',
+    unique: 'O campo {{field}} é único e é existente',
+  };
+  public reporter = validator.reporters.api;
+  public cacheKey = this.ctx.routeKey;
+}
---
+++ app/Validators/UserValidator.ts
@@ -0,0 +1,24 @@
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import BaseValidator from './BaseValidator';
+
+export default class UserValidator extends BaseValidator {
+  constructor(protected ctx: HttpContextContract) {
+    super(ctx);
+  }
+
+  public schema = schema.create({
+    username: schema.string([
+      rules.unique({ table: 'utilizadores', column: 'username' }),
+      rules.minLength(3),
+      rules.maxLength(21),
+      rules.alphaNum(),
+      rules.regex(/([a-zA-Z]+)([a-zA-Z]+|[\d]*)/),
+    ]),
+    nome: schema.string([rules.minLength(3), rules.maxLength(45)]),
+    sobrenome: schema.string([rules.minLength(3), rules.maxLength(45)]),
+    foto: schema.string([rules.minLength(4), rules.maxLength(255)]),
+    password: schema.string([rules.minLength(6), rules.maxLength(255)]),
+    email: schema.string([rules.email(), rules.unique({ table: 'utilizadores', column: 'email' })]),
+  });
+}

d4169fd40d5d42ef08f624533030a9b6166470c1
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-19 10:48:59 -0300

    feat #4: Update validators

--- app/Validators/UUIDValidator.ts
+++ app/Validators/UUIDValidator.ts
@@ -2,18 +2,12 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import BaseValidator from './BaseValidator';
 
-export default class UsernameValidator extends BaseValidator {
+export default class UUIDValidator extends BaseValidator {
   constructor(protected ctx: HttpContextContract) {
     super(ctx);
   }
 
   public schema = schema.create({
-    username: schema.string([
-      rules.minLength(3),
-      rules.maxLength(21),
-      rules.alphaNum(),
-      rules.trim(),
-      rules.regex(/^[a-zA-Z0-9_-]{3,16}$/),
-    ]),
+    uuid: schema.string([rules.uuid({ version: 4 })]),
   });
 }

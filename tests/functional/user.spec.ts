fd27f150e402943f020797da4ae5e5a7e07f520e
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-19 10:48:26 -0300

    feat #4: Add the unit testing for find an user

--- tests/functional/user.spec.ts
+++ tests/functional/user.spec.ts
@@ -1,5 +1,6 @@
 import Database from '@ioc:Adonis/Lucid/Database';
 import { test } from '@japa/runner';
+import { v4 } from 'uuid';
 import UtilizadorFactory from 'Database/factories/UtilizadorFactory';
 
 test.group('Testar a funcionalidades relacionadas ao utilizador', (group) => {
@@ -8,13 +9,12 @@ test.group('Testar a funcionalidades relacionadas ao utilizador', (group) => {
 
   test('Criar um utilizador', async ({ client }) => {
     const utilizador = await UtilizadorFactory.create();
-    // console.log(utilizador.$attributes);
     const response = await client.post('/api/user').json(utilizador);
     response.assertStatus(201);
   });
 
-  // test('Procurar um utilizador pelo username', async ({ client }) => {
-  //   const response = await client.get(`/api/user/${user.username}`).json(user);
-  //   response.assertStatus(200);
-  // });
+  test('Procurar um utilizador pelo uid e não encontrar', async ({ client }) => {
+    const response = await client.get(`/api/user/${v4()}`);
+    response.assertStatus(404);
+  });
 });

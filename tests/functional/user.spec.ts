32b72ac54874f9fb188a1afc5e173323caaeb7bd
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-08-19 16:57:49 -0300

    chore: Housekeeping

--- tests/functional/user.spec.ts
+++ tests/functional/user.spec.ts
@@ -9,40 +9,40 @@ test.group('Testar a funcionalidades relacionadas ao utilizador', (group) => {
   group.each.setup(async () => await Database.beginGlobalTransaction());
   group.each.teardown(async () => await Database.rollbackGlobalTransaction());
 
-  // test('Criar um utilizador', async ({ client }) => {
-  //   const utilizador: UserType = createFakeUser();
-  //   const response = await client.post('/api/user').json(utilizador);
-  //   response.assertStatus(201);
-  // });
+  test('Criar um utilizador', async ({ client }) => {
+    const utilizador: UserType = createFakeUser();
+    const response = await client.post('/api/user').json(utilizador);
+    response.assertStatus(201);
+  });
 
-  // test('Procurar um utilizador pelo uid e não encontrar', async ({ client }) => {
-  //   const response = await client.get(`/api/user/${v4()}`);
-  //   response.assertStatus(404);
-  // });
+  test('Procurar um utilizador pelo uid e não encontrar', async ({ client }) => {
+    const response = await client.get(`/api/user/${v4()}`);
+    response.assertStatus(404);
+  });
 
-  // test('Procurar um utilizador pelo uid retornar os dados', async ({ client }) => {
-  //   const utilizador: UserType = createFakeUser();
-  //   const request = await client.post('/api/user').json(utilizador);
-  //   const uid = request.response.body.uid;
-  //   const response = await client.get(`/api/user/${uid}`);
-  //   response.assertStatus(200);
-  // });
+  test('Procurar um utilizador pelo uid retornar os dados', async ({ client }) => {
+    const utilizador: UserType = createFakeUser();
+    const request = await client.post('/api/user').json(utilizador);
+    const uid = request.response.body.uid;
+    const response = await client.get(`/api/user/${uid}`);
+    response.assertStatus(200);
+  });
 
-  // test('Atualizar os dados de um utilizador que não existe e retorna 404', async ({ client }) => {
-  //   const utilizador: UserType = createFakeUser();
-  //   const response = await client.put(`/api/user/${v4()}`).json(utilizador);
-  //   response.assertStatus(404);
-  // });
+  test('Atualizar os dados de um utilizador que não existe e retorna 404', async ({ client }) => {
+    const utilizador: UserType = createFakeUser();
+    const response = await client.put(`/api/user/${v4()}`).json(utilizador);
+    response.assertStatus(404);
+  });
 
-  // test('Atualizar os dados de um utilizador existente', async ({ client }) => {
-  //   let utilizador: UserType = createFakeUser();
-  //   const request = await client.post('/api/user').json(utilizador);
-  //   const uid = request.response.body.uid;
-  //   utilizador.email = faker.internet.email();
-  //   utilizador.nome = faker.person.fullName();
-  //   const response = await client.put(`/api/user/${uid}`).json(utilizador);
-  //   response.assertStatus(200);
-  // });
+  test('Atualizar os dados de um utilizador existente', async ({ client }) => {
+    let utilizador: UserType = createFakeUser();
+    const request = await client.post('/api/user').json(utilizador);
+    const uid = request.response.body.uid;
+    utilizador.email = faker.internet.email();
+    utilizador.nome = faker.person.fullName();
+    const response = await client.put(`/api/user/${uid}`).json(utilizador);
+    response.assertStatus(200);
+  });
 
   test('Eliminar os dados de um utilizador existente', async ({ client }) => {
     let utilizador: UserType = createFakeUser();
